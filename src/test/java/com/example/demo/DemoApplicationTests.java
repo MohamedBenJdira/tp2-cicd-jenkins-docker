602d89db384289799fbe577ac1687b5d9db80cc2
Hama02 <mohamed.ben.jdira@mindbay.ai>
2025-10-16 09:34:33 +0100

    Initial TP2 app

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoApplicationTests {
+
+  @Test
+  void add() {
+    assertEquals(4, 2 + 2);
+  }
+}
